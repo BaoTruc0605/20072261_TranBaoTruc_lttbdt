ca22b00b3d67e527a65fcdb4b8866be3bb0fc28f
Bảo Trúc <114350927+BaoTruc0605@users.noreply.github.com>
2023-09-12 18:20:00 +0700

    TH1_Lan2
    update_Lan2

--- TH1/BMI.js
+++ TH1/BMI.js
@@ -1,11 +1,13 @@
 function calculateBMI() {
     var massMark = parseInt(document.getElementById("massMark").value)
-    var heightMark = parseInt(document.getElementById("heightMark").value)
+    let heightMark = parseFloat(document.getElementById("heightMark").value)
     var massJohn = parseInt(document.getElementById("massJohn").value)
-    var heightJohn = parseInt(document.getElementById("heightJohn").value)
-    let bmiMark = (massMark / ((heightMark * heightMark) / 10000)).toFixed(2)
-    let bmiJohn = (massJohn / ((heightJohn * heightJohn) / 10000)).toFixed(2)
+    let heightJohn = parseFloat(document.getElementById("heightJohn").value)
+    let bmiMark = (massMark / (heightMark * heightMark) ).toFixed(2)
+    let bmiJohn = (massJohn / (heightJohn * heightJohn) ).toFixed(2)
     document.getElementById('BMIMark').value = bmiMark;
     document.getElementById('BMIJohn').value = bmiJohn;
+    let markHigherBMI = bmiMark > bmiJohn;
+    document.getElementById('result').value = markHigherBMI;
 
 }
